cec73f9f2077b7d99fc21ceefb628f87099672f7
Sethurajan27 <106911055+Sethurajan27@users.noreply.github.com>
2023-04-21 13:05:06 +0530

    Update get-last-page.js

--- prelnk/get-last-page.js
+++ prelnk/get-last-page.js
@@ -1,6 +1,3 @@
 const previousPage = document.getElementById('previous-page');
 
 previousPage.textContent = document.referrer;
-
-var myParagraph1 = document.getElementById("start").innerHTML;
-console.log("Using innerHTML: " + myParagraph1);
