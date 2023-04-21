8b708b8f897199c6f536b591dd720886b182d1bd
Sethurajan27 <106911055+Sethurajan27@users.noreply.github.com>
2023-04-21 13:05:20 +0530

    Update history.js

--- prelnk/history.js
+++ prelnk/history.js
@@ -16,6 +16,8 @@ fetch("history.html")
 		var concatenatedText = text1 + text2;
 		
 		// Use the innerHTML property to set the text of the HTML element to the concatenated text
-		document.getElementById("concatenated-text").innerHTML = concatenatedText;
+		document.getElementById("start ").innerHTML = concatenatedText;
+
+		myParagraph.textContent = concatenatedText;
 	})
 	.catch(error => console.log(error));
